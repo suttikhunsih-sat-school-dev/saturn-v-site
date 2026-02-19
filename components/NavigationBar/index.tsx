5880faeca89444bf8ddd82c9ce548923e4f41a43
Meme Hofman <waewarisa.sae@saturday-school.org>
2026-02-19 14:01:27 +0700

    fix home page

--- components/NavigationBar/index.tsx
+++ components/NavigationBar/index.tsx
@@ -1,43 +1,43 @@
 const menus = {
-  "เกี่ยวกับ Saturn V": [
-    ["/about", "Saturn V คืออะไร"],
-    ["/theme", "Theme Saturn V"],
+  'เกี่ยวกับ SATURN V': [
+    ['/about', 'SATURN V คืออะไร'],
+    ['/theme', 'Theme SATURN V'],
   ],
-  "โครงการของ SATURN V": [
-    ["/project", "Saturn V Project"],
-  ],
-  "สนใจทำโครงการ": [
-    ["/format", "รูปแบบการจัดโครงการ"],
-    ["/target", "ระยะเวลาการจัดโครงการและกลุ่มเป้าหมายในการจัดโครงการ"],
-    ["/pr", "Public Relations Saturn V"],
-    ["/report", "การรายงานผลหลังจบโครงการ (Report)"],
+  'โครงการของ SATURN V': [['/project', 'SATURN V Project']],
+  สนใจทำโครงการ: [
+    ['/format', 'รูปแบบการจัดโครงการ'],
+    ['/target', 'ระยะเวลาการจัดโครงการและกลุ่มเป้าหมายในการจัดโครงการ'],
+    ['/pr', 'Public Relations SATURN V'],
+    ['/report', 'การรายงานผลหลังจบโครงการ (Report)'],
   ],
 };
 
 export default function NavigationBar() {
   return (
-    <nav className="bg-sat-school-primary-yellow text-black mx-auto w-fit rounded-xl font-bold text-3xl py-4 px-[100px] shadow-lg -mt-3 relative z-20">
+    <nav className="bg-sat-school-primary-yellow text-black mx-auto w-fit rounded-xl font-bold text-3xl py-4 px-[100px] shadow-lg -mt-6 relative z-20">
       <ul className="flex gap-[60px]">
-        {Object.entries(menus).map(([title, items]) => (
-          <li key={title} className="relative group">
-            <button className="hover:text-sat-school-dark-blue bg-transparent">
-              {title}
-            </button>
+        {Object.entries(menus).map(([title, items], index) => {
+          return (
+            <li key={`${title}-${index}`} className="relative group">
+              <button className="hover:text-sat-school-dark-blue bg-transparent [text-shadow:1_1px_0_#808080] tracking-wider">
+                {title}
+              </button>
 
-            <ul className="absolute hidden group-hover:block hover:block bg-white rounded-lg shadow-lg overflow-hidden text-xl font-normal left-1/2 -translate-x-1/2 min-w-[220px]">
-              {items.map(([link, label]) => (
-                <li key={link}>
-                  <a
-                   href={link}
-                   className="block w-full px-6 py-3 hover:bg-gray-100 hover:text-sat-school-dark-blue"
-                  >
-                  {label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+              <ul className="absolute hidden group-hover:block hover:block bg-white rounded-lg shadow-lg overflow-hidden text-xl font-normal left-1/2 -translate-x-1/2 min-w-[220px]">
+                {items.map(([link, label], index) => (
+                  <li key={`${link}-${index}`}>
+                    <a
+                      href={link}
+                      className="block w-full px-6 py-3 hover:bg-gray-100 hover:text-sat-school-dark-blue"
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
