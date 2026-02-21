8cb1c869f64cdf87bd012748b29fb78d8cc7fc44
Suttikhun Sihsobhon <khun2551@hotmail.com>
2026-02-21 15:51:45 +0700

    add esport page

--- app/project/esports/page.tsx
+++ app/project/esports/page.tsx
@@ -1,7 +1,25 @@
+import SubProjectPageContent from '../../../components/ProjectPage/SubProjectPageContent';
+
 export default function Esports() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Esports</h1>
-    </div>
+    <SubProjectPageContent
+      logoList={[
+        { src: '/logo-main-black.png', alt: 'Logo sat school black' },
+        { src: '/logo3.svg', alt: 'Logo saturn v black' },
+      ]}
+      pageTitle="Saturday School Saturn V Project"
+      pageSubtitle="Garena ห้องเรียน ESPORTS"
+      projectObjective="บริษัท การีนา ออนไลน์ (ประเทศไทย) จำกัด ได้ร่วมมือกับมูลนิธิโรงเรียนวันเสาร์ในการพัฒนาหลักสูตรห้องเรียน Esports จำนวน 40 ชั่วโมง  ที่ครูสามารถนำไปใช้สอนในโรงเรียนได้ทั้งในวิชาหลัก วิชาเสริม หรือชั่วโมงกิจกรรม       โดยครูและบุคคลทั่วไปสามารถใช้ได้ฟรี และมีจัดการแข่งขัน Esports เพื่อชิงทุนการศึกษาโดยได้รับทุนการศึกษาสำหรับการเรียนต่อระดับปริญญาตรีที่มหาวิทยาลัยธุรกิจบัณฑิตย์ ในคณะด้านนิเทศศาสตร์ และ Esports โดยไม่มีค่าใช้จ่าย"
+      projectContent1="ให้น้องๆได้เรียนรู้เกี่ยวกับความหลากหลายของอาชีพในวงการอีสปอร์ต เช่น นักพากย์ โค้ช สื่อ นักข่าว และผู้จัดการแข่งขัน"
+      postProjectTitle="สิ่งที่นักเรียนจะได้หลังจากจบโครงการ"
+      postProjectOutcome="ได้รับทุนการศึกษาจากมหาวิทยาลัยธุรกิจบัณฑิตย์ และได้รับทักษะทั้งด้าน Hard skills เช่น ทักษะการจัดแข่ง Esport tournament ทักษะการใช้ภาษาในการพากย์ การทำ Artwork รวมถึง Soft Skills เช่น ความคิดแบบพัฒนาได้ (Growth Mindset)"
+      participantNumbers={{
+        students: 2000,
+        volunteers: 50,
+        teachers: 10,
+      }}
+      imageGruopSrc="/esportImage.png"
+      videoSrc="https://www.youtube.com/embed/yqYhS3ncMrY"
+    />
   );
 }
