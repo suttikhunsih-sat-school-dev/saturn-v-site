b093c158dea093eed2ea0c12087dbc39dd60c22c
suttikhunsih-sat-school-dev <suttikhun.sih@saturday-school.org>
2026-02-21 16:47:06 +0700

    Implement all project page (#10)
    * add new page until growth mindset
    
    * Done base projects all
    
    ---------
    
    Co-authored-by: Suttikhun Sihsobhon <khun2551@hotmail.com>

--- app/project/media-literacy/page.tsx
+++ app/project/media-literacy/page.tsx
@@ -1,7 +1,25 @@
+import SubProjectPageContent from '../../../components/ProjectPage/SubProjectPageContent';
+
 export default function MediaLiteracy() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Media Literacy</h1>
-    </div>
+    <SubProjectPageContent
+      logoList={[
+        { src: '/logo-main-black.png', alt: 'Logo sat school black' },
+        { src: '/logo3.svg', alt: 'Logo saturn v black' },
+      ]}
+      pageTitle="Saturday School Saturn V Project"
+      pageSubtitle="โครงการ ส่งเสริมการรู้เท่าทันสื่อออนไลน์ (Media Literacy)"
+      projectObjective="สำนักการศึกษา กรุงเทพมหานคร ร่วมมือกับ มูลนิธิโรงเรียนวันเสาร์ และ TikTok เพื่อเสริมสร้างทักษะการรู้เท่าทันและใช้สื่อสังคมออนไลน์อย่างปลอดภัยและสร้างสรรค์ในหมู่นักเรียนระดับมัธยมศึกษา"
+      projectContent1="สร้างทักษะการคิดวิเคราะห์สื่ออย่างมีวิจารณญาณ ส่งเสริมการเป็นผู้ใช้และผู้ผลิตสื่อที่มีความรับผิดชอบต่อสังคมช่วยพัฒนาความคิดแบบพัฒนาได้ (Growth Mindset)"
+      postProjectTitle="สิ่งที่นักเรียนจะได้หลังจากจบโครงการ"
+      postProjectOutcome="นักเรียนมีความรู้ความเข้าใจเรื่องการรู้เท่าทันสื่อบนโลกออนไลน์มากขึ้น มีความกล้าแสดงออก และมีสติในการใช้สื่ออย่างปลอดภัย"
+      participantNumbers={{
+        students: 500,
+        volunteers: 35,
+        teachers: 10,
+      }}
+      imageGruopSrc="/media-literacy-group-image.png"
+      videoSrc="https://www.youtube.com/embed/yqYhS3ncMrY"
+    />
   );
 }
--- app/project/signature-workshop/page.tsx
+++ app/project/signature-workshop/page.tsx
@@ -1,7 +1,24 @@
+import SubProjectPageContent from '../../../components/ProjectPage/SubProjectPageContent';
+
 export default function SignatureWorkshop() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Signature Workshop</h1>
-    </div>
+    <SubProjectPageContent
+      logoList={[
+        { src: '/logo-main-black.png', alt: 'Logo sat school black' },
+        { src: '/logo3.svg', alt: 'Logo saturn v black' },
+      ]}
+      pageTitle="Saturday School Saturn V Project"
+      pageSubtitle="Signature Workshop"
+      projectObjective="ห้องเรียนเวิร์กช็อปสำหรับผู้ที่เชื่อว่า ห้องเรียนไม่ควรมีแค่คำตอบที่ถูกต้อง แต่เป็นพื้นที่ที่ปลอดภัยสำหรับการเรียนรู้และเติบโตของทุกคนเหมาะสำหรับผู้อยากสร้างการเปลี่ยนแปลงในห้องเรียน หลักคิดที่ไม่เพียงแต่ช่วยให้กล้าลองผิดลองถูก เรียนรู้จากความล้มเหลว แต่ยังช่วยให้ได้ทบทวนวิธีคิดของตนเอง เปิดใจต่อความหลากหลาย และนำไปสู่ห้องเรียนด้วยพลังใหม่"
+      projectContent1="เข้าใจหลักการของ Growth Mindset และเห็นภาพการนำไปใช้จริง"
+      postProjectTitle="สิ่งที่เด็กจะได้ประโยชน์"
+      postProjectOutcome="ได้ฝึกวิธีการสื่อสารที่ช่วย สร้างแรงจูงใจในการเรียนรู้ของเด็ก และได้แลกเปลี่ยนประสบการณ์และไอเดียกับผู้เข้าร่วม"
+      participantNumbers={{
+        students: 14,
+        volunteers: 20,
+        teachers: 25,
+      }}
+      imageGruopSrc="/signature-workshop-image-group.png"
+    />
   );
 }
